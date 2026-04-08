2fbfa734eb58f81ac50ff29c3e0637b2b963fac1
j2wicee <joshuaevans2x@gmail.com>
2026-04-07 22:13:42 -0400

    trying to fix some accumulus errors with js

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { SiteFooter } from "./SiteFooter";
 import { SiteHeader } from "./SiteHeader";
 
 export function MainLayout() {
+  useEffect(() => {
+    const existing = document.querySelector('script[data-lint-page="true"]');
+    if (existing) return;
+
+    const script = document.createElement("script");
+    script.src = "https://lint.page/kit/4d0fe3.js";
+    script.crossOrigin = "anonymous";
+    script.dataset.lintPage = "true";
+    document.head.appendChild(script);
+  }, []);
+
   return (
     <>
       <SiteHeader />
